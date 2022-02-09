7888464ed188e3008c11d20b9b1a4379f876418f
Fernando <fernando.montero.perez@gmail.com>
2022-02-09 13:13:04 +0000

    added auth context

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,32 @@ import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 const queryClient = new QueryClient()
 import './i18n/config'
 import { useTranslation } from 'react-i18next'
+import { AuthProvider } from './context/auth-context'
 function App() {
     const { t } = useTranslation()
     return (
         <QueryClientProvider client={queryClient}>
-            <div className="App">
-                <header className="App-header">
-                    <h1 className="text-5xl font-bold underline">
-                        Hello world!
-                    </h1>
-                    <h2>{t('title')}</h2>
-                    <p>{t('description.part1')}</p>
-                    <p>{t('description.part2')}</p>
-                    <p>{t('description.part3')}</p>
-                </header>
-                <nav>
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                </nav>
-                <Routes>
-                    <Route path="/" element={<p>home</p>} />
-                    <Route path="about" element={<p>About </p>} />
-                </Routes>
-            </div>
+            <AuthProvider>
+                <div className="App">
+                    <header className="App-header">
+                        <h1 className="text-5xl font-bold underline">
+                            Hello world!
+                        </h1>
+                        <h2>{t('title')}</h2>
+                        <p>{t('description.part1')}</p>
+                        <p>{t('description.part2')}</p>
+                        <p>{t('description.part3')}</p>
+                    </header>
+                    <nav>
+                        <Link to="/">Home</Link>
+                        <Link to="/about">About</Link>
+                    </nav>
+                    <Routes>
+                        <Route path="/" element={<p>home</p>} />
+                        <Route path="about" element={<p>About </p>} />
+                    </Routes>
+                </div>
+            </AuthProvider>
         </QueryClientProvider>
     )
 }
