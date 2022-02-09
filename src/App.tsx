67aadf71b37f86db60db1cb49947415a017cab8c
Fernando <fernando.montero.perez@gmail.com>
2022-02-09 12:29:52 +0000

    added react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1 className="text-5xl font-bold underline">Hello world!</h1>
-            </header>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<p>home</p>} />
-                <Route path="about" element={<p>About </p>} />
-            </Routes>
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="text-5xl font-bold underline">
+                        Hello world!
+                    </h1>
+                </header>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<p>home</p>} />
+                    <Route path="about" element={<p>About </p>} />
+                </Routes>
+            </div>
+        </QueryClientProvider>
     )
 }
 
