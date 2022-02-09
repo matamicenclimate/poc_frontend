b468df561ec2d951385322b854295d467bfa473f
Fernando <fernando.montero.perez@gmail.com>
2022-02-09 12:55:41 +0000

    added i18n

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import './App.css'
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
 const queryClient = new QueryClient()
-
+import './i18n/config'
+import { useTranslation } from 'react-i18next'
 function App() {
+    const { t } = useTranslation()
     return (
         <QueryClientProvider client={queryClient}>
             <div className="App">
@@ -13,6 +15,10 @@ function App() {
                     <h1 className="text-5xl font-bold underline">
                         Hello world!
                     </h1>
+                    <h2>{t('title')}</h2>
+                    <p>{t('description.part1')}</p>
+                    <p>{t('description.part2')}</p>
+                    <p>{t('description.part3')}</p>
                 </header>
                 <nav>
                     <Link to="/">Home</Link>
