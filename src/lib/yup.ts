1c13ca9b59f5133bea16ee7c4a3abadb043f552d
Fernando <fernando.montero.perez@gmail.com>
2022-02-22 14:00:52 +0000

    feat(i18n): transalted validation

---
+++ src/lib/yup.ts
@@ -0,0 +1,16 @@
+import * as yup from 'yup';
+
+yup.setLocale({
+  // use constant translation keys for messages without values
+
+  mixed: {
+    default: 'validation.errors.required',
+    required: 'validation.errors.required',
+  },
+
+  // use functions to generate an error object that includes the value from the schema
+  number: {
+    min: ({ min }) => ({ key: 'field_too_short', values: { min } }),
+    max: ({ max }) => ({ key: 'field_too_big', values: { max } }),
+  },
+});
