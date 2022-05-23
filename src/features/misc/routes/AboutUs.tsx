ada54284a689899212b15e00de06a87d5f6c473a
Fernando Montero <fernando.montero.perez@gmail.com>
2022-05-23 16:36:57 +0200

    fix: dummy about page

--- src/features/misc/routes/AboutUs.tsx
+++ src/features/misc/routes/AboutUs.tsx
@@ -1,11 +1,5 @@
 import { MainLayout } from '@/componentes/Layout/MainLayout';
-import { PageTitle } from '@/componentes/Layout/PageTitle';
 
 export const AboutUs = () => {
-  return (
-    <MainLayout title="About us">
-      <PageTitle title="About us" description="More info" />
-      asd
-    </MainLayout>
-  );
+  return <MainLayout title="About us">asd</MainLayout>;
 };
