020f4928301e523e8b359ae4d46a78b21cbddcbc
Fernando Montero <fernando.montero.perez@gmail.com>
2022-06-14 13:24:26 +0100

    chore: validation cleanup

--- src/features/compensations/validation/CompensateValidation.ts
+++ src/features/compensations/validation/CompensateValidation.ts
@@ -1,8 +1,5 @@
 import * as yup from 'yup';
 
-const DOCUMENTS_SUPPORTED_FORMATS = ['application/pdf'];
-const IMAGES_SUPPORTED_FORMATS = ['image/png', 'image/jpg', 'image/jpeg'];
-
 export const calculateCompensationValidation = yup.object({
   wallet: yup.object().nullable().required(),
   amount: yup.number().required(),
